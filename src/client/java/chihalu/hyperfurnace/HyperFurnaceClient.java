30b775cac75245bc33c802231d56fda67bfbb54e
Chihalu <m.chihiro0314@gmail.com>
2025-10-22 16:03:34 +0900

    first commit

---
+++ src/client/java/chihalu/hyperfurnace/HyperFurnaceClient.java
@@ -0,0 +1,10 @@
+package chihalu.hyperfurnace;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HyperFurnaceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
